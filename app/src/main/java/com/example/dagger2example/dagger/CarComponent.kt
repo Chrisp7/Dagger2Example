351d1a96c9c1bc12f2e55acdc36eaec62329b539
Pat Chen <chenpeng@microsoft.com>
2021-09-17 18:06:55 +0800

    complete

--- app/src/main/java/com/example/dagger2example/dagger/CarComponent.kt
+++ app/src/main/java/com/example/dagger2example/dagger/CarComponent.kt
@@ -1,12 +1,11 @@
 package com.example.dagger2example.dagger
 
 import com.example.dagger2example.MainActivity
-import com.example.dagger2example.car.Car
 import dagger.Component
 
 @Component(
     dependencies = [EngineComponent::class]
 )
 interface CarComponent {
-    fun getCar(): Car
+    fun inject(mainActivity: MainActivity)
 }
